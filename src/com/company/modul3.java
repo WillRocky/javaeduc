1fc2e03d5269be79e55c9d2617cf466e03e560f3
Rock <rockwe2r4@gmail.com>
2021-11-02 16:25:29 +0200

    close

--- src/com/company/modul3.java
+++ src/com/company/modul3.java
@@ -1,8 +1,38 @@
 package com.company;
-
+import java.util.Scanner;
 public class modul3 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter array length:");
+        int size = scanner.nextInt();
+        int array[] = new int[size];
+        System.out.println("Insert array elements: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println();
+        int n = array.length;
+        int temp;
+        for (int i =0; i<n/2; i++ ) {
+            temp = array[n - i - 1];
+            array[n - i - 1] = array[i];
+            array[i] = temp;
+        }
+        for (int i =0; i<array.length; i++) {
+            //System.out.print("Перевернули массив");
+            System.out.print("  " + array[i]);
+        }
+
+
+
+
+            //  System.out.print("Inerested array elemeents:");
+            //for (int i = 0; i<size;i++) {
+            //  System.out.print(" " +array [i] );
+
+
+        }
+
 
     }
-}
+
--- src/com/company/practica22.java
+++ src/com/company/practica22.java
@@ -3,7 +3,7 @@ public class practica22 {
     public static void main(String[] args) {
         String str = String.format("Человек по имени  %s,  живет в городе %s", "Богдан", "Глухов");
         System.out.println(str);
-        String stg = String.format("Этому человеку %s и любит он заниматься %s ", "20", "футболом");
+        String stg = String.format("Этому человеку %s и любит он заниматься %s ", "21", "футболом");
         System.out.println(stg);
 
     }
